417d92f2f1eacb26bf38822dcdb2a3bc25770a77
gnouv-fpt <228469372+gnouv-fpt@users.noreply.github.com>
2026-01-29 10:40:40 +0700

    feat: Add eureka, dependency & product service

---
+++ productservice/src/main/java/com/productservice/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.productservice.controller;
+
+import com.productservice.entity.Product;
+import com.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return service.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return service.getProductById(id);
+    }
+}
---
+++ productservice/src/main/java/com/productservice/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.productservice.service;
+
+import com.productservice.entity.Product;
+import com.productservice.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+    public Product saveProduct(Product product) {
+        return repository.save(product);
+    }
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+}
