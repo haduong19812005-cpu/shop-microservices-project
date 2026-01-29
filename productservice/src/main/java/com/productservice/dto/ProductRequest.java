382a67424ac4d7c07515d1e1b02c7fc49be34d39
gnouv-fpt <228469372+gnouv-fpt@users.noreply.github.com>
2026-01-29 13:58:18 +0700

    feat: Add dto request and response of product

---
+++ productservice/src/main/java/com/productservice/dto/ProductRequest.java
@@ -0,0 +1,37 @@
+package com.productservice.dto;
+
+public class ProductRequest {
+    private String name;
+    private double price;
+    private int stockQuantity;
+    public ProductRequest() {}
+
+    public ProductRequest(String name, double price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
+
